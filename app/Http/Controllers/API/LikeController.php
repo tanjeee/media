a2c7665b59ca6651c24e05dbc6ff9827f0bb9505
Tan Jing En <jingen0463@gmail.com>
2023-12-14 16:51:10 +0800

    eight commit

--- app/Http/Controllers/API/LikeController.php
+++ app/Http/Controllers/API/LikeController.php
@@ -6,29 +6,50 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Like;
+use App\Models\Post;
 
 class LikeController extends Controller
 {
-    public function like(Request $request){
-        $like = Like::where('post_id',$request->id)->where('user_id',Auth::user()->id)->get();
+    public function like(Request $request)
+    {
+        $like = Like::where('post_id', $request->id)->where('user_id', Auth::user()->id)->first();
 
-        //check if it returns 0 then this post is not liked and should be liked or unliked
-        if(count($like)>0){
-            //prevent like more than 1 time
-            $like[0]->delete();
+        // Check if the post is already liked
+        if ($like) {
+            // Prevent liking more than once
+            $like->delete();
+
+            // Update the like count on the post
+            $post = Post::find($request->id);
+            if ($post) {
+                $post->likes_count = $post->likes_count > 0 ? $post->likes_count - 1 : 0;
+                $post->save();
+            }
+
             return response()->json([
                 'success' => true,
-                'message' => 'Unliked'
+                'message' => 'Unliked',
+                'count' => $post->likes_count,
             ]);
         }
 
+        // If not liked, create a new like
         $like = new Like;
         $like->user_id = Auth::user()->id;
         $like->post_id = $request->id;
         $like->save();
+
+        // Update the like count on the post
+        $post = Post::find($request->id);
+        if ($post) {
+            $post->likes_count += 1;
+            $post->save();
+        }
+
         return response()->json([
             'success' => true,
-            'message' => 'Liked'
+            'message' => 'Liked',
+            'count' => $post->likes_count,
         ]);
     }
 }
