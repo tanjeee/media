07ead26fe915873d14652cf7b13497a09afafe56
Tan Jing En <jingen0463@gmail.com>
2023-12-14 17:07:23 +0800

    eight commit

--- app/Http/Controllers/API/LikeController.php
+++ app/Http/Controllers/API/LikeController.php
@@ -10,7 +10,7 @@ use App\Models\Like;
 class LikeController extends Controller
 {
     public function like(Request $request){
-        $like = Like::where('post_id',$request->id)->where('user_id',Auth::user()->id)->get();
+        $like = Like::where('post_id',$request->id)->where('user_id',Auth::user()->id)->first();
 
         //check if it returns 0 then this post is not liked and should be liked or unliked
         if($like){
