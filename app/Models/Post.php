a94ae0f732b52860580a5d967d0adc6cefaaf6fe
Tan Jing En <jingen0463@gmail.com>
2023-12-14 15:46:22 +0800

    seven commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Comment;
+use App\Models\Like;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,5 +13,10 @@ class Post extends Model
     public function comments()
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
     }
 }
