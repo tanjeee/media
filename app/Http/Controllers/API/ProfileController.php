6c29c2541458f19a390772997ed1545f3375a8f8
Tan Jing En <jingen0463@gmail.com>
2023-12-16 17:33:43 +0800

    eight commit

--- app/Http/Controllers/API/ProfileController.php
+++ app/Http/Controllers/API/ProfileController.php
@@ -6,65 +6,54 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 use Illuminate\Http\UploadedFile;
 
 class ProfileController extends Controller
 {
-    public function update_profile(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'profile_photo' => 'nullable|image|mimes:jpg,png,bmp',
-        ]);
+    public function update_profile(Request $request, $id)
+{
+    $validator = Validator::make($request->all(), [
+        'profile_photo' => 'nullable|image|mimes:jpg,png,bmp',
+    ]);
 
-        if ($validator->fails()) {
-            return response()->json([
-                'message' => 'Validation fails',
-                'errors' => $validator->errors(),
-            ], 422);
-        }
+    if ($validator->fails()) {
+        return response()->json([
+            'message' => 'Validation fails',
+            'errors' => $validator->errors(),
+        ], 422);
+    }
 
-        $user = auth()->user();
+    $user = auth()->user();
 
-        if ($request->hasFile('profile_photo')) {
-            if ($user->profile_photo) {
-                $old_path = public_path('images') . $user->profile_photo;
-                if (File::exists($old_path)) {
-                    File::delete($old_path);
-                }
+    if ($request->hasFile('profile_photo')) {
+        if ($user->profile_photo) {
+            $old_path = public_path('images') . $user->profile_photo;
+            if (File::exists($old_path)) {
+                File::delete($old_path);
             }
-
-            $image = $request->file('profile_photo');
-            $image_name = time() . '.' . $image->getClientOriginalExtension();
-
-            $image->move(public_path('images'), $image_name);
-        } else {
-            $image_name = $user->profile_photo;
         }
 
-        $user->update([
-            'profile_photo' => $image_name,
-        ]);
+        $image = $request->file('profile_photo');
+        $image_name = time() . '.' . $image->getClientOriginalExtension();
 
-        $imageUrl = url('images/' . $image_name);
-
-        return response()->json([
-            'message' => 'Profile successfully updated',
-            'user' => $user,
-            'image_url' => $imageUrl,
-        ], 200);
+        $image->move(public_path('images'), $image_name);
+        $user->profile_photo = $image_name;
+    } else {
+        $image_name = $user->profile_photo;
     }
 
-    public function get_profile(Request $request)
-    {
-        $user = auth()->user();
+    $user->name = $request->name;
+    $user->save();
 
-        return response()->json([
-            'user' => [
-                'id' => $user->id,
-                'name' => $user->name,
-                'email' => $user->email,
-                'profile_photo' => $user->profile_photo,
-            ],
-        ]);
-    }
+    $imageUrl = url('images/' . $image_name);
+
+    return response()->json([
+        'message' => 'Profile successfully updated',
+        'user' => $user,
+        'image_url' => $imageUrl,
+    ], 200);
+}
+
+
 }
