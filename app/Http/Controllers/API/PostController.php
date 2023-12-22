17b90abb7786198ea7caa81e283ff1daf64d1ce2
Tan Jing En <jingen0463@gmail.com>
2023-12-22 22:28:03 +0800

    eight commit

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -7,12 +7,10 @@ use App\Http\Controllers\Controller;
 use Auth;
 use Illuminate\Http\Request;
 use Storage;
-use Log;
 
 class PostController extends Controller
 {
     public function create(Request $request){
-        Log::info('Received request data: ' . json_encode($request->all()));
         $post = new Post;
         $post->user_id = Auth::user()->id;
         $post->desc = $request->desc;
@@ -23,6 +21,7 @@ class PostController extends Controller
             file_put_contents('storage/posts/'.$photo,base64_decode($request->photo));
             $post->photo = $photo;
         }
+        
         $post->save();
         $post->user;
 
