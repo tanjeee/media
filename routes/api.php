769be478fba12e37b32a3c3384618e7313e4bbc9
Tan Jing En <jingen0463@gmail.com>
2023-12-24 20:58:58 +0800

    eight commit

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,7 @@ Route::middleware('auth:api')->group(function () {
 Route::post('posts/create', [PostController::class, 'create']);
 Route::post('posts/delete', [PostController::class, 'delete']);
 Route::post('posts/update', [PostController::class, 'update']);
-Route::get('posts', [PostController::class, 'posts']);
+Route::posts('posts', [PostController::class, 'posts']);
 
 //comment
 Route::post('comments/create', [CommentController::class, 'create']);
